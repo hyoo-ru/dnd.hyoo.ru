5da6a6c33bf23d2590b45ad6f360849b81b07838
nin-jin <sairi-na-tenshi@ya.ru>
2025-04-13 21:58:39 +0000

    Deploying to gh-pages from @ hyoo-ru/dungeon.hyoo.ru@ba222465209aef789d804692f1286d3ca2cbba88 🚀

--- node.mjs
+++ node.mjs
@@ -7986,11 +7986,12 @@ var $;
                 ])];
         }
         inventory(next) {
-            return this.value('inventory', next) ?? [...new Set([
+            const def = [...new Set([
                     ...this.class().inventory,
                     ...this.race().inventory,
                     ...this.story().inventory,
                 ])].join('\n');
+            return this.value('inventory', next) ?? def;
         }
         moral(next) {
             return this.value('moral', next) ?? 'neutral';
