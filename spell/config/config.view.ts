b15aef666543ee62ee449b3401c942b8923cb818
jin <jin@hyoo.ru>
2025-04-20 21:29:28 +0300

    ++

--- spell/config/config.view.ts
+++ spell/config/config.view.ts
@@ -14,18 +14,18 @@ namespace $.$$ {
 			return this.$.$hyoo_dungeon_damage_all[ id ]?.title ?? '???'
 		}
 		
-		@ $mol_mem
-		override classes_options() {
-			return Object.keys( this.$.$hyoo_dungeon_class_all )
-		}
+		// @ $mol_mem
+		// override classes_options() {
+		// 	return Object.keys( this.$.$hyoo_dungeon_class_all )
+		// }
 		
-		override class_title( id: $hyoo_dungeon_class_id ) {
-			return this.$.$hyoo_dungeon_class_all[ id ].title
-		}
+		// override class_title( id: $hyoo_dungeon_class_id ) {
+		// 	return this.$.$hyoo_dungeon_class_all[ id ].title
+		// }
 		
-		override class_checked( id: $hyoo_dungeon_class_id, next?: boolean ) {
-			return this.spell().class_has( id, next )
-		}
+		// override class_checked( id: $hyoo_dungeon_class_id, next?: boolean ) {
+		// 	return this.spell().class_has( id, next )
+		// }
 		
 		override component_checked( id: 'verbal' | 'somatic' , next?: boolean ) {
 			return this.spell().component( id, next )
