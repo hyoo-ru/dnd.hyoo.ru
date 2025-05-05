a83f7df61ec441e04208fddbbf89dba7919d98ca
nin-jin <nin-jin@users.noreply.github.com>
2025-05-05 07:14:12 +0000

    Deploying to gh-pages from @ hyoo-ru/dnd.hyoo.ru@b5b8a517149ec4e6be47ee9cc5bb84ef517e64bf 🚀

--- node.test.js
+++ node.test.js
@@ -12316,7 +12316,7 @@ var $;
 			return obj;
 		}
 		type(){
-			return "tel";
+			return "text";
 		}
 		value_string(next){
 			if(next !== undefined) return next;
@@ -12335,6 +12335,7 @@ var $;
 		String(){
 			const obj = new this.$.$mol_string();
 			(obj.type) = () => ((this.type()));
+			(obj.keyboard) = () => ("decimal");
 			(obj.value) = (next) => ((this.value_string(next)));
 			(obj.hint) = () => ((this.hint()));
 			(obj.enabled) = () => ((this.string_enabled()));
--- web.mjs
+++ web.mjs
@@ -11852,7 +11852,7 @@ var $;
 			return obj;
 		}
 		type(){
-			return "tel";
+			return "text";
 		}
 		value_string(next){
 			if(next !== undefined) return next;
@@ -11871,6 +11871,7 @@ var $;
 		String(){
 			const obj = new this.$.$mol_string();
 			(obj.type) = () => ((this.type()));
+			(obj.keyboard) = () => ("decimal");
 			(obj.value) = (next) => ((this.value_string(next)));
 			(obj.hint) = () => ((this.hint()));
 			(obj.enabled) = () => ((this.string_enabled()));
