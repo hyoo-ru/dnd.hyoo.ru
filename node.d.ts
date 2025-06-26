5c9eeff291fc91a26d78123855b6ed68e88657f4
nin-jin <nin-jin@users.noreply.github.com>
2025-06-26 07:14:37 +0000

    Deploying to gh-pages from @ hyoo-ru/dnd.hyoo.ru@50c59498ca81fe1a18e2ce0c94c75c927b957ae6 🚀

--- node.d.ts
+++ node.d.ts
@@ -7415,7 +7415,9 @@ declare namespace $.$$ {
 declare namespace $ {
 
 	export class $mol_theme_auto extends $mol_plugin {
-		theme( ): string
+		dark( ): string
+		theme( ): ReturnType< $mol_theme_auto['dark'] >
+		light( ): string
 		attr( ): ({ 
 			'mol_theme': ReturnType< $mol_theme_auto['theme'] >,
 		}) 
@@ -7426,7 +7428,7 @@ declare namespace $ {
 //# sourceMappingURL=auto.view.tree.d.ts.map
 declare namespace $.$$ {
     class $mol_theme_auto extends $.$mol_theme_auto {
-        theme(): "$mol_theme_light" | "$mol_theme_dark";
+        theme(): string;
     }
 }
 
